dd7457f28e169c41e59e5d5f2724c8c447aa3cb3
burnpiro <kemalpiro@gmail.com>
2021-02-11 22:23:07 +0100

    training helpers

--- training/datasets.py
+++ training/datasets.py
@@ -5,17 +5,18 @@ import pandas as pd
 import numpy as np
 from skimage import io, transform
 from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 from torchvision import transforms, utils
 
 
-def get_edible_plants_data(base_path="../data/Edible wild plants/datasets"):
+def get_edible_plants_data(base_path):
     train_base = os.path.join(base_path, "dataset")
 
     train_df = pd.DataFrame(columns=["image", "label", "class"])
     classes = []
     i = 0
     for dir_name in os.listdir(train_base):
-        className = "-".join(dir_name.split(" "))
+        className = "-".join(dir_name.split(" ")).lower()
         classes.append(className)
 
         images = []
@@ -35,7 +36,7 @@ def get_edible_plants_data(base_path="../data/Edible wild plants/datasets"):
 
     test_df = pd.DataFrame(columns=["image", "label", "class"])
     for dir_name in os.listdir(test_base):
-        className = "-".join(dir_name.split(" "))
+        className = "-".join(dir_name.split(" ")).lower()
 
         images = []
         for class_dir in os.scandir(os.path.join(test_base, dir_name)):
@@ -55,7 +56,12 @@ def get_edible_plants_data(base_path="../data/Edible wild plants/datasets"):
 class EdibleWildPlantsDataset(Dataset):
     """Edible wild plants dataset."""
 
-    def __init__(self, root_dir, transform=None, data_type="train"):
+    def __init__(
+        self,
+        root_dir="../data/Edible wild plants/datasets",
+        transform=None,
+        data_type="train",
+    ):
         """
         Args:
             root_dir (string): Directory with all the images.
@@ -67,19 +73,35 @@ class EdibleWildPlantsDataset(Dataset):
         self.data = train_df if data_type == "train" else test_df
         self.root_dir = root_dir
         self.transform = transform
+        self._classes_df = self.data.drop_duplicates(subset=["class"])
 
     def __len__(self):
+        """
+        :return: int
+        """
         return len(self.data)
 
+    @property
+    def classes(self):
+        """
+        Get list of classes
+
+        :return List[string]
+        """
+        return self._classes_df.label.unique()
+
     def __getitem__(self, idx):
+        """
+        :param idx: int
+        :return: Tuple(array(3,244,244), int)
+        """
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        image = io.imread(self.data.iloc[idx, ["image"]])
-        class_num = self.data.iloc[idx, ["class"]]
-        sample = {"image": image, "class": class_num}
+        row = self.data.iloc[idx]
+        img = Image.open(row["image"]).convert('RGB')
 
         if self.transform:
-            sample = self.transform(sample)
+            img = self.transform(img)
 
-        return sample
+        return img, int(row["class"])
--- training/helpers.py
+++ training/helpers.py
@@ -2,10 +2,11 @@ import torch
 import time
 import os
 import copy
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=25):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def train_model(model, dataloaders, criterion, optimizer, scheduler, device, num_epochs=25):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -29,6 +30,9 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=
 
             # Iterate over data.
             for inputs, labels in dataloaders[phase]:
+                # print(inputs.shape)
+                # print(labels.shape)
+                # break
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
@@ -74,3 +78,43 @@ def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs=
     # load best model weights
     model.load_state_dict(best_model_wts)
     return model
+
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy()
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def visualize_model(model, dataloaders, device, class_names, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
+
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
